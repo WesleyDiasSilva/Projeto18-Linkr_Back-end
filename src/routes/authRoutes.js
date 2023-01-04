878e7ca2eae1b1d917226c578688fd28670d0ec1
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2023-01-04 17:44:14 -0300

    feat: logout, delete session

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { loginController } from "../controllers/authControllers/loginController.js";
 import { loginMiddleware } from "../middlewares/authMiddlewares/loginMiddleware.js";
+import { signUpMiddleware } from "../middlewares/authMiddlewares/signUpMiddleware.js"; 
+import { postSignUp } from "../controllers/authControllers/signUpController.js";
+import { logoutController } from "../controllers/authControllers/logoutController.js";
 
 const route = Router();
 
 route.post("/sign-in", loginMiddleware, loginController);
+
+route.post("/sign-up", signUpMiddleware, postSignUp);
+
+route.post("/logout", logoutController);
 
 export default route;
