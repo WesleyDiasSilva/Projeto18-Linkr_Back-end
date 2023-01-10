58baf6612107790eac7d706e92600167c1859256
Gabriel Chiavegatto <98192895+gabriel-chiavegatto@users.noreply.github.com>
2023-01-10 02:20:48 -0300

    Revert "Refact erros"

--- src/repositories/posts/deletePost.js
+++ src/repositories/posts/deletePost.js
@@ -1,13 +1,12 @@
 import { db } from "../../database/connection.js";
 
-export async function deletePost(postId, userId) {
+export async function deletePost(postId) {
     try {
-        console.log("POSTID E USERID", postId,userId)
         await db.query(`
-        DELETE FROM posts
-        WHERE id = $1 AND user_id = $2;
-        `, [postId, userId]);
-        return { status: true }
+            DELETE FROM posts
+            WHERE user_id = $1;
+        `, [postId]);
+        return { status: true}
     } catch (error) {
         return { status: false, error: error }
     }
