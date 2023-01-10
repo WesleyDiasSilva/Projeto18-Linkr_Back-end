1c040c7851c3840bfa9146e272ce8a6d9b8c8b98
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2023-01-10 02:08:21 -0300

    feat: delete post

--- src/repositories/posts/deletePost.js
+++ src/repositories/posts/deletePost.js
@@ -1,12 +1,13 @@
 import { db } from "../../database/connection.js";
 
-export async function deletePost(postId) {
+export async function deletePost(postId, userId) {
     try {
+        console.log("POSTID E USERID", postId,userId)
         await db.query(`
-            DELETE FROM posts
-            WHERE user_id = $1;
-        `, [postId]);
-        return { status: true}
+        DELETE FROM posts
+        WHERE id = $1 AND user_id = $2;
+        `, [postId, userId]);
+        return { status: true }
     } catch (error) {
         return { status: false, error: error }
     }
