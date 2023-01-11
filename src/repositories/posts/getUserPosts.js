914c8ecaf87b9828270ddcd588c7c353d991d101
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2023-01-11 01:57:42 -0300

    feat: query userPosts

--- src/repositories/posts/getUserPosts.js
+++ src/repositories/posts/getUserPosts.js
@@ -2,10 +2,33 @@ import { db } from "../../database/connection.js";
 
 export async function getUserPosts(id) {
     try {
+        const limitPosts = 40;
         const posts = await db.query(`
-            SELECT * FROM posts
-            WHERE user_id = $1
-        `, [id]);
+        SELECT 
+            p.id, 	
+            p.user_id, 
+            p.link, 
+            p.description, 
+            p.created_at, 
+            COUNT(l.post_id) AS "Number_of_likes",
+            (select id from likes where user_id=p.user_id and post_id=p.id) as "youLiked",
+            u.username, 
+            u.picture_url
+        FROM users u
+        JOIN posts p
+            ON p.user_id = u.id
+            LEFT JOIN posts_trendings t
+                ON t.post_id = p.id
+            LEFT JOIN likes l
+                ON l.post_id = p.id
+        WHERE u.id=$1
+        GROUP BY 
+				p.id, 
+				u.username, 
+				u.picture_url
+			ORDER BY p.id desc
+			LIMIT $2;
+        `, [id, limitPosts]);
         console.log(id,posts)
         return { rows: posts.rows, error: false }
     } catch (error) {
